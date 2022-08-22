1490eecd7a2eeb2b3651ca0f38dffc53f9de0265
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2022-08-22 20:28:10 +0300

    some updates

--- js/hiddenFilter.js
+++ js/hiddenFilter.js
@@ -1,3 +1,9 @@
 $('.products__filter-show').click(() => {
     $('.products__filter-hidden').slideToggle('slow')
+    if ($('.products__filter-show').text() == 'Скрыть') {
+        $('.products__filter-show').html('Показать все фильтры <span class="products__filter-show--value">12</span>')
+    }
+    else {
+        $('.products__filter-show').html('Скрыть')
+    }
 })
